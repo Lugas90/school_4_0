1df9869ec056a3f52c991a7cd8d3463bf4e00533
Lugas90 <Lugas90@mail.ru>
2022-10-12 18:34:31 +0300

    1 комит

---
+++ src/main/java/ru/hogvarts/school_4_0/School40Application.java
@@ -0,0 +1,13 @@
+package ru.hogvarts.school_4_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class School40Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(School40Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hogvarts/school_4_0/School40ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogvarts.school_4_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class School40ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
